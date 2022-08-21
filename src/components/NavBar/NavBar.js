b069ee99123cb93d4e626751fb2d5b4a0e0f1db5
unknown <asanzluis@gmail.com>
2022-08-21 21:45:29 +0200

    nav

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="navegador">
+          <ul>
+            <li><a href=""></a>Perros </li>
+            <li><a href=""></a>Gatos</li>
+            <li><a href=""></a>Ofertas</li>
+            <li><a href=""></a>Nosotros</li>
+          </ul>
+        </nav>
+    
+    )
+  };
+  
+  export default Navbar;
